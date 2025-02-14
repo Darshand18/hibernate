cc172e3d7ecb8709177d9c686c2533059420bbcc
Darshan D <dd656804@gmail.com>
2025-02-14 10:49:52 +0530

    Day11

---
+++ hibernate-001/src/main/java/Utilities/SessionFactoryProvider.java
@@ -0,0 +1,14 @@
+package Utilities;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class SessionFactoryProvider {
+    public static SessionFactory provideSessionFactory()
+    {
+        Configuration config = new Configuration();
+        config.configure("resources/hibernate.cfg.xml");
+        return config.buildSessionFactory();
+
+    }
+}
